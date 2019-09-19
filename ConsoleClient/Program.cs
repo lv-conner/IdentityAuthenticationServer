76f4aa3e2cc94bbeb113a8be1f7ee0be99995ab8
lv-conner <lvmh123456@gmail.com>
2019-09-19 15:34:29 +0800

    validate issuer

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,8 +1,15 @@
 ﻿using System;
+using System.IdentityModel.Tokens.Jwt;
 using System.Net.Http;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using IdentityModel.Client;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json.Linq;
+using System.Linq;
+using IdentityModel;
+using System.Collections.Generic;
 
 namespace ConsoleClient
 {
@@ -35,7 +42,35 @@ namespace ConsoleClient
             }
             else
             {
-                Console.WriteLine(passwordTokenResponse.Json);
+                var keyClient = new HttpClient()
+                {
+                };
+                var webkey = await keyClient.GetJsonWebKeySetAsync("http://localhost:5000/.well-known/openid-configuration/jwks");
+                if(webkey.IsError)
+                {
+                    return;
+                }
+                var configuration = OpenIdConnectConfiguration.Create(webkey.Raw);
+                var RsaParameter = new RSAParameters()
+                {
+                    Exponent = Convert.FromBase64String(webkey.KeySet.Keys.First().E),
+                    Modulus = Base64Url.Decode(webkey.KeySet.Keys.First().N)
+                };
+
+                var rsa = new RSACryptoServiceProvider(2048);
+                var publishKey = rsa.ExportParameters(false);
+                var key = new RsaSecurityKey(RsaParameter);
+                var validateParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                {
+                    ValidateIssuerSigningKey = false,
+                    IssuerSigningKey = key,
+                    ValidateAudience = false,
+                    ValidateIssuer = false
+                };
+
+                JwtSecurityTokenHandler jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+                var user = jwtSecurityTokenHandler.ValidateToken(passwordTokenResponse.Json.GetValue("access_token").ToString(), validateParameters, out var token);
+                Console.WriteLine(passwordTokenResponse.Json.GetValue("access_token"));
             }
             Console.WriteLine("\n\n");
             Console.ReadKey();
---
+++ WebClient/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebClient.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly TokenClient _tokenClient;
+        public AccountController(TokenClient _tokenClient)
+        {
+
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ActionName("Login")]
+        public async Task<IActionResult> LoginAsync(string userName, string password)
+        {
+            var tokenResponse = await _tokenClient.RequestPasswordTokenAsync(userName, password);
+            if(tokenResponse.IsError)
+            {
+                return Content(tokenResponse.ErrorDescription);
+            }
+            else
+            {
+                var use = tokenResponse.AccessToken;
+            }
+            return View();
+        }
+    }
+}
